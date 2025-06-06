54ba4cd8ca81d2bce281c8418279b7ea1da65958
Kxanx1538 <121854477+Kxanx1538@users.noreply.github.com>
2025-06-06 05:36:56 +0000

    Fix:forms

--- src/components/pages/Bookings/index.js
+++ src/components/pages/Bookings/index.js
@@ -1,4 +1,4 @@
-import { act, useReducer } from 'react';
+import { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 import { fetchAPI, submitAPI } from '../../../utils/fakeAPI';
